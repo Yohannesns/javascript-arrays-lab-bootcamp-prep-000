b0072422a791ee2e3ce6d7d0995633df843d6991
Yohannes Abera <yohannesns@gmail.com>
2018-05-27 23:06:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ function destructivelyRemoveFirstKitten(name){
 //
 // }
  function removeFirstKitten(name){
-   return kittens.slice(1,kittens.length-1)
+   return kittens.slice(1,kittens.length)
  }
